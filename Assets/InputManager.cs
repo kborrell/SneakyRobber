f3a07a6b1484c3dcea46029b2624a944b8e542d0
kborrell <kborrells@gmail.com>
2017-12-02 17:02:03 +0100

    Implemented basic movement.

---
+++ Assets/InputManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager {
+
+    public enum Button
+    {
+        None,
+        Left,
+        Right,
+        Up,
+        Down,
+        Action
+    }
+
+    public bool GetButton(Button button)
+    {
+        if (button == Button.Left)
+        {
+            return Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        }
+        else if (button == Button.Right)
+        {
+            return Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+        }
+        else if (button == Button.Up)
+        {
+            return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        }
+        else if (button == Button.Down)
+        {
+            return Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        }
+        else if (button == Button.Action)
+        {
+            return Input.GetKey(KeyCode.E);
+        }
+
+        return false;
+    }
+
+    public bool GetButtonUp(Button button)
+    {
+        if (button == Button.Left)
+        {
+            return Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow);
+        }
+        else if (button == Button.Right)
+        {
+            return Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow);
+        }
+        else if (button == Button.Up)
+        {
+            return Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow);
+        }
+        else if (button == Button.Down)
+        {
+            return Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow);
+        }
+        else if (button == Button.Action)
+        {
+            return Input.GetKeyUp(KeyCode.E);
+        }
+
+        return false;
+    }
+
+    public bool GetButtonDown(Button button)
+    {
+        if (button == Button.Left)
+        {
+            return Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow);
+        }
+        else if (button == Button.Right)
+        {
+            return Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow);
+        }
+        else if (button == Button.Up)
+        {
+            return Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+        }
+        else if (button == Button.Down)
+        {
+            return Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow);
+        }
+        else if (button == Button.Action)
+        {
+            return Input.GetKeyDown(KeyCode.E);
+        }
+
+        return false;
+    }
+
+    private InputManager()
+    {
+
+    }
+
+    private Stack<KeyCode> m_pressedKeys = new Stack<KeyCode>();
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (s_instance == null)
+            {
+                s_instance = new InputManager();
+            }
+
+            return s_instance;
+        }
+    }
+
+    private static InputManager s_instance;
+}
---
+++ Assets/MovementComponent.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour {
+
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_transform = GetComponent<Transform>();
+
+        m_directionsMap[InputManager.Button.Right] = Vector3.right;
+        m_directionsMap[InputManager.Button.Left] = Vector3.left;
+        m_directionsMap[InputManager.Button.Up] = Vector3.forward;
+        m_directionsMap[InputManager.Button.Down] = Vector3.back;
+    }
+
+    private void Update()
+    {
+        if (InputManager.Instance.GetButtonDown(InputManager.Button.Right))
+        {
+            m_pressedButtons.Push(InputManager.Button.Right);
+            m_currentDirection = m_directionsMap[InputManager.Button.Right];
+        }
+        else if (InputManager.Instance.GetButtonDown(InputManager.Button.Left))
+        {
+            m_pressedButtons.Push(InputManager.Button.Left);
+            m_currentDirection = m_directionsMap[InputManager.Button.Left];
+        }
+        else if (InputManager.Instance.GetButtonDown(InputManager.Button.Up))
+        {
+            m_pressedButtons.Push(InputManager.Button.Up);
+            m_currentDirection = m_directionsMap[InputManager.Button.Up];
+        }
+        else if (InputManager.Instance.GetButtonDown(InputManager.Button.Down))
+        {
+            m_pressedButtons.Push(InputManager.Button.Down);
+            m_currentDirection = m_directionsMap[InputManager.Button.Down];
+        }
+
+        if (m_pressedButtons.Count > 0 && InputManager.Instance.GetButtonUp(m_pressedButtons.Peek()))
+        {
+            while (m_pressedButtons.Count > 0 && !InputManager.Instance.GetButton(m_pressedButtons.Peek()))
+            {
+                m_pressedButtons.Pop();
+            }
+
+            if (m_pressedButtons.Count > 0)
+            {
+                m_currentDirection = m_directionsMap[m_pressedButtons.Peek()];
+            }
+            else
+            {
+                m_currentDirection = Vector3.zero;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigidBody.MovePosition(m_transform.position + m_currentDirection * m_playerSpeed * Time.deltaTime);
+    }
+
+    private void OnGUI()
+    {
+        printInputStackStatus();
+    }
+
+    void printInputStackStatus()
+    {
+        string status = "STACK: [";
+        if (m_pressedButtons.Count > 0)
+        {
+            foreach (InputManager.Button button in m_pressedButtons)
+            {
+                status = status + button.ToString() + ", ";
+            }
+        }
+        status = status + "]";
+        GUILayout.Label(status);
+    }
+
+    public float m_playerSpeed;
+    public Dictionary<InputManager.Button, Vector3> m_directionsMap = new Dictionary<InputManager.Button, Vector3>();
+
+    private Stack<InputManager.Button> m_pressedButtons = new Stack<InputManager.Button>();
+    private Vector3   m_currentDirection;
+
+    private Transform m_transform;
+    private Rigidbody m_rigidBody;
+}
