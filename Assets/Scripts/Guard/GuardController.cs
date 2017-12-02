18a20bfcad67c4121f5c2fde9118490ea234a1ca
Alexander <alexandru.borsevici@starloopstudios.com>
2017-12-03 00:00:41 +0100

    Add Environment scene with basic GuardController

---
+++ Assets/Scripts/Guard/GuardController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GuardController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform flashlight;
+
+    [SerializeField]
+    private State currentState;
+
+    public enum State
+    {
+        Patrol,
+        Wonder,
+        Alert,
+        Chase
+    }
+
+    private void Start()
+    {
+        StartCoroutine(FlashlightLookAnimation());
+    }
+
+    IEnumerator FlashlightLookAnimation()
+    {
+        while (true)
+        {
+            float angle = (Random.value * 60) - 30;
+            yield return flashlight.DOLocalRotate(new Vector3(5, angle, 0), 1f).SetEase(Ease.OutSine);
+            yield return new WaitForSeconds(Random.Range(1f,3f));
+        }
+    }
+}
---
+++ Assets/Scripts/Guard/PatrolPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPoint : MonoBehaviour
+{
+    [SerializeField]
+    Color groupColor = Color.yellow;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = groupColor;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+}
