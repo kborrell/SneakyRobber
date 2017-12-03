363c21e2f53fe6c7548b675338c1c0fa4a8c0674
kborrell <kborrells@gmail.com>
2017-12-03 21:41:09 +0100

    Added 3D text when collecting coins.

--- Assets/Scripts/TreasureInteraction.cs
+++ Assets/Scripts/TreasureInteraction.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(PlayerData))]
 public class TreasureInteraction : MonoBehaviour {
@@ -49,10 +50,26 @@ public class TreasureInteraction : MonoBehaviour {
             {
                 var treasure = treasuresList[i];
                 m_playerData.AddScore(treasure.GetValue());
+                StartCoroutine(ShowScore(i, treasure.GetValue()));
             }
 
             m_playerData.ClearInventory();
         }   
+    }
+
+    IEnumerator ShowScore(int index, int value)
+    {
+        yield return new WaitForSeconds(index * 0.4f);
+
+        var text = Instantiate(m_scoreTextMesh) as TextMesh;
+        text.text = value.ToString();
+        text.transform.parent = transform;
+        text.transform.localPosition = new Vector3(Random.Range(-2.0f, 2.0f), 2.5f, 0);
+
+        text.transform.DOLocalMoveY(3.5f, 1.0f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            Destroy(text.gameObject);
+        });
     }
 
     void OnInventoryItemSelected(int index)
@@ -94,4 +111,6 @@ public class TreasureInteraction : MonoBehaviour {
 
     private PlayerData m_playerData;
     private Transform  m_transform;
+
+    public TextMesh m_scoreTextMesh;
 }
