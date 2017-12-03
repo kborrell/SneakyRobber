488b08f0570828fe05c9e5b040bf4cf54efad48d
Alexander <alexandru.borsevici@starloopstudios.com>
2017-12-03 18:35:33 +0100

    Add patrol waypoints

---
+++ Assets/Scripts/Guard/Editor/PatrolRouteEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PatrolRoute))]
+public class PatrolRouteEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PatrolRoute patrol = (PatrolRoute)target;
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("+"))
+        {
+            patrol.AddPoint();
+        }
+        if (GUILayout.Button("-"))
+        {
+            patrol.RemovePoint();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+}
