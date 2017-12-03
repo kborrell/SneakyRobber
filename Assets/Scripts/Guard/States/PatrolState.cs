59eaef46e0fae490d7ca9cf5f061aee31820169c
Alexander <alexandru.borsevici@starloopstudios.com>
2017-12-03 18:35:56 +0100

    Add Guard and patrol state

---
+++ Assets/Scripts/Guard/States/PatrolState.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine.AI;
+
+public class PatrolState : IState
+{
+    NavMeshAgent navAgent;
+    PatrolRoute route;
+    PatrolPoint point;
+
+    public PatrolState(GuardController guard)
+    {
+        navAgent = guard.NavAgent;
+    }
+
+    public void Enter()
+    {
+        PatrolRoute newRoute = PatrolRoute.GetNewRoute();
+        navAgent.isStopped = false;
+
+        if(newRoute != route)
+        {
+            route = newRoute;
+            point = null;
+        }
+
+        GotoPoint(point);
+    }
+
+    public void Exit()
+    {
+        if(route != null)
+        {
+            route.DismissGuard();
+        }
+
+        navAgent.isStopped = true;
+    }
+
+    public void Update()
+    {
+        // Navigate
+        if (!navAgent.pathPending && navAgent.remainingDistance < 0.5f)
+        {
+                GotoPoint(route.GetNextPoint(point));
+        }
+
+        // Detect
+
+    }
+
+    private void GotoPoint(PatrolPoint targetPoint)
+    {
+        if (targetPoint != null)
+        {
+            navAgent.destination = targetPoint.transform.position;
+            point = targetPoint;
+        }
+        else
+        {
+            route = PatrolRoute.GetNewRoute(route);
+        }
+    }
+}
